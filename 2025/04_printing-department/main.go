dfea8c60f474d3fa13932f21ded57fec693c1714
Akhdan Fadhilah <me@akhdan.dev>
2025-12-04 20:25:13 +0900

    [25.4] part 2 reduce gc pressure

--- 2025/04_printing-department/main.go
+++ 2025/04_printing-department/main.go
@@ -79,6 +79,22 @@ var directions = [8][2]int{
 	{-1, -1}, // up-left
 }
 
+func countAdjacentRolls(grid [][]byte, r, c int) int {
+	count := 0
+	for _, dir := range directions {
+		nr, nc := r+dir[0], c+dir[1]
+		if nr >= 0 && nr < len(grid) &&
+			nc >= 0 && nc < len(grid[r]) &&
+			isRoll(grid[nr][nc]) {
+			count++
+			if count >= 4 {
+				break // stop early
+			}
+		}
+	}
+	return count
+}
+
 // brute force to the rescue haha
 func partOne(grid [][]byte) (int, error) {
 	resultChan := make(chan int, len(grid))
@@ -90,21 +106,7 @@ func partOne(grid [][]byte) (int, error) {
 				if !isRoll(grid[r][c]) {
 					continue
 				}
-
-				adjacentRolls := 0
-				for _, dir := range directions {
-					nr, nc := r+dir[0], c+dir[1]
-					if nr >= 0 && nr < len(grid) &&
-						nc >= 0 && nc < len(grid[r]) &&
-						isRoll(grid[nr][nc]) {
-						adjacentRolls++
-						if adjacentRolls >= 4 {
-							break // stop early
-						}
-					}
-				}
-
-				if adjacentRolls < 4 {
+				if countAdjacentRolls(grid, r, c) < 4 {
 					result++
 				}
 			}
@@ -121,7 +123,7 @@ func partOne(grid [][]byte) (int, error) {
 }
 
 func partTwo(grid [][]byte) (int, error) {
-	// read and store where rolls are
+	// read and store where rolls are (initially)
 	rolls := make([][2]int, 0)
 	for r := range len(grid) {
 		for c := range len(grid[r]) {
@@ -133,26 +135,14 @@ func partTwo(grid [][]byte) (int, error) {
 
 	// while loop until there is no roll to remove
 	result := 0
+	removed := make([][2]int, len(rolls)) // preallocate
+	stayed := make([][2]int, len(rolls))
 	for {
-		removed := make([][2]int, 0)
-		stayed := make([][2]int, 0)
+		removed, stayed = removed[:0], stayed[:0] // reset slices
 
 		// check all rolls in current iteration
 		for _, pos := range rolls {
-			r, c := pos[0], pos[1]
-			adjacentRolls := 0
-			for _, dir := range directions {
-				nr, nc := r+dir[0], c+dir[1]
-				if nr >= 0 && nr < len(grid) &&
-					nc >= 0 && nc < len(grid[r]) &&
-					isRoll(grid[nr][nc]) {
-					adjacentRolls++
-					if adjacentRolls >= 4 {
-						break // stop early
-					}
-				}
-			}
-			if adjacentRolls < 4 {
+			if countAdjacentRolls(grid, pos[0], pos[1]) < 4 {
 				removed = append(removed, pos)
 			} else {
 				stayed = append(stayed, pos)
@@ -166,11 +156,13 @@ func partTwo(grid [][]byte) (int, error) {
 
 		// update rolls and grid for next iteration
 		fmt.Printf("Removed %d from %d rolls\n", len(removed), len(rolls))
-		rolls = stayed
 		for _, pos := range removed {
 			grid[pos[0]][pos[1]] = '.' // mark as removed
 		}
 		result += len(removed)
+		rolls, stayed = stayed, rolls
+		// we do swap here because stayed will be reset in next slices
+		// if we do rolls = stayed, then rolls itself will be lost
 	}
 	return result, nil
 }
